1c4b9dee18a71651999f24133834df1e0197705a
Tamir Karssli <Tamir Karssli>
2016-05-18 01:44:45 -0700

    Removed whitespace

--- Reader.java
+++ Reader.java
@@ -1,42 +1,44 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Scanner;
 
 public class Reader {
     Hashtable<String, String> dict;
+
     public static void main(String[] args) throws IOException {
 
         // Create new Reader object
         Reader reader = new Reader();
 
         // Main loop, get a string input from user
-        while(true){
+        while (true) {
             System.out.println();
             System.out.print("Enter a word to be spellchecked: ");
-
             Scanner inputReader = new Scanner(System.in);
             String input = inputReader.nextLine();
+
+            // Get back list of corrections
             ArrayList<String> results = reader.checkWord(input);
 
             // UI conditionals
-            if(results.size() <1){
+            if (results.size() < 1) {
                 System.out.println("No mistakes found");
-            } else{
+            } else {
 
                 System.out.println("    Incorrectly spelled word, Corrections:");
 
-                for(String s : results){
+                for (String s : results) {
                     System.out.println("        " + s);
-
                 }
-
             }
         }
-
     }
 
-    private Reader(){
+    private Reader() {
 
         // Create dictionary as hashtable from .txt file
         try {
@@ -45,22 +47,22 @@ public class Reader {
             BufferedReader dictFile = new BufferedReader(new FileReader("C:\\Projects\\Alias HW\\linked lists\\src\\words.txt"));
 
             // Add words line by line until end of file
-            while(dictFile.ready()){
+            while (dictFile.ready()) {
                 String word = dictFile.readLine();
                 dict.put(word, word);
             }
             dictFile.close();
 
-        }catch(IOException e){
+        } catch (IOException e) {
             System.out.print("Error on creating dictionary, file not found or bad  file");
         }
     }
 
-    public ArrayList<String> checkWord(String word){
+    public ArrayList<String> checkWord(String word) {
         ArrayList<String> corrections = new ArrayList<String>();
 
         // If dict has word, it is spelled correctly so return
-        if(dict.containsKey(word)){
+        if (dict.containsKey(word)) {
             return corrections;
         }
 
@@ -68,48 +70,47 @@ public class Reader {
         ArrayList<String> edits = possibleEdits(word);
 
         // Check if edits are correctly spelled words add to a list
-        for(String s : edits){
-            if(dict.containsKey(s)){
-             corrections.add(s);
+        for (String s : edits) {
+            if (dict.containsKey(s)) {
+                corrections.add(s);
             }
         }
 
         // If no corrections exist, add string stating so to list
-        if(corrections.size() < 1){
+        if (corrections.size() < 1) {
             corrections.add("      No corrections found");
         }
 
         return corrections;
     }
 
-    private ArrayList<String> possibleEdits(String word){
+    private ArrayList<String> possibleEdits(String word) {
         ArrayList<String> edits = new ArrayList<String>();
 
 
         // Add letter to beginning
-        for( int i =0; i<26;i++){
-            String letter = Character.toString((char) (97+i));
-            edits.add( letter + word );
+        for (int i = 0; i < 26; i++) {
+            String letter = Character.toString((char) (97 + i));
+            edits.add(letter + word);
         }
 
         // Add letter to end
-        for( int i =0; i<26;i++){
-            String letter = Character.toString((char) (97+i));
-            edits.add( word + letter );
+        for (int i = 0; i < 26; i++) {
+            String letter = Character.toString((char) (97 + i));
+            edits.add(word + letter);
         }
 
         // Remove character from beginning
-        edits.add(word.substring(1,word.length()));
+        edits.add(word.substring(1, word.length()));
 
         // Remove character from the end
-        edits.add(word.substring(0,word.length()-1));
+        edits.add(word.substring(0, word.length() - 1));
 
         // Exchange adjacent characters
-        for( int i=0; i< word.length()-1; i++){
-            edits.add(word.substring(0,i) + word.substring(i+1,i+2) + word.substring(i,i+1) + word.substring(i+2));
+        for (int i = 0; i < word.length() - 1; i++) {
+            edits.add(word.substring(0, i) + word.substring(i + 1, i + 2) + word.substring(i, i + 1) + word.substring(i + 2));
         }
 
         return edits;
-
     }
 }
